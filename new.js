7b1e0a11155427748e3f072c3329cc9546a18536
santhosh-07-sandy <santhosh1917santo@gmail.com>
2024-11-24 11:08:07 +0530

    Added new.js file

---
+++ new.js
@@ -0,0 +1,8 @@
+function convert()
+    {
+       let cmVal = Number(document.getElementById("input").value)
+       let inchVal = cmVal/2.5
+
+       let result = document.getElementById("result")
+       result.innerHTML = inchVal.toFixed(2) + " inches"
+     }
